0a28be9b15bd12a9e3952e79b824eed1ba736e8a
Reza <rezammdzd0@gmail.com>
2024-12-30 22:42:25 +0330

    feat: create dynamic route to show post (slug)

---
+++ src/app/blogs/[postSlug]/page.jsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+const SinglePost = async ({ params }) => {
+  // await new Promise((res) => setTimeout(() => res(), 3000));
+  const param = await params;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/post/slug/${param.postSlug}`
+  );
+  const { data } = await res.json();
+  const { post } = data || {};
+  if (!post) notFound();
+  return (
+    <div className="text-secondary-600 max-w-screen-md mx-auto">
+      <h1 className="text-secondary-700 text-2xl font-bold mb-8">
+        {post.title}
+      </h1>
+      <p className="mb-4">{post.briefText}</p>
+      <p className="mb-8">{post.text}</p>
+      <div className="relative aspect-video aspect-h-9 overflow-hidden rounded-lg mb-10">
+        <Image
+          className="object-cover object-center hover:scale-110 transition-all ease-out duration-300"
+          fill
+          src={post.coverImageUrl}
+          alt={post.title}
+        />
+      </div>
+      {/* {post.related.length > 0 ? <RelatedPost posts={post.related} /> : null} */}
+      {/* <BlogComments post={post} /> */}
+    </div>
+  );
+};
+
+export default SinglePost;
