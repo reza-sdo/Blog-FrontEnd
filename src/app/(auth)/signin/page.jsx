e7128d6c7090c7d38e4af7a4c2a77576ef9222e6
Reza <rezammdzd0@gmail.com>
2025-01-19 22:51:43 +0330

    feat: add TextField component
    2- add sigin page
    3- add signup page

---
+++ src/app/(auth)/signin/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SigninPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default SigninPage
---
+++ src/app/(auth)/signup/page.jsx
@@ -0,0 +1,17 @@
+import TextField from '@/ui/TextField';
+
+export const metadata = {
+  title: 'ثبت نام',
+};
+const SignupPage = () => {
+  return (
+    <div>
+      <h1>ثبت نام</h1>
+      <form>
+        {/* <TextField name="name" label="نام و نام خانوادگی" isRequired onChange={} value={} /> */}
+      </form>
+    </div>
+  );
+};
+
+export default SignupPage;
