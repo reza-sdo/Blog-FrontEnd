f75818a2146f90b168cb1b5b162c52565e3b847e
Reza <rezammdzd0@gmail.com>
2025-02-11 22:25:36 +0330

    feat: add getUserData

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,3 +7,7 @@ export async function signupApi(data) {
 export async function signinApi(data) {
   return http.post('/user/signin', data).then(({ data }) => data.data);
 }
+
+export async function getUserApi(data) {
+  return http.get('/user/profile', data).then(({ data }) => data.data);
+}
