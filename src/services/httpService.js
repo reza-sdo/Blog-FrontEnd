1a2011272c24a3aaf5f2357156614cf5ba41a0e8
Reza <rezammdzd0@gmail.com>
2025-03-01 22:57:07 +0330

    feat: handle refresh token

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -5,6 +5,30 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+api.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/user/refresh-token`,
+          { withCredentials: true }
+        );
+        if (data) return api(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+  }
+);
+
 const http = {
   get: api.get,
   post: api.post,
